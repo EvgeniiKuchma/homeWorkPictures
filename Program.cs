446742e565d3cf169781bae77823455d9905df99
Евгений Кучма <desferto87@gmail.com>
2024-12-10 13:32:25 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWorkPictures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int allUserPicts = 52;
+            int pictsInRow = 3;
+            int numberFullRows;
+            int residuePicts;
+
+            numberFullRows = allUserPicts / pictsInRow;
+            residuePicts = numberFullRows % pictsInRow;
+
+            Console.WriteLine($"Number full rows it's {numberFullRows}, pictures residue {residuePicts}");
+        }
+    }
+}
